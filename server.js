243664e095b99347a8154539fd512ea4fe18ec02
Mathew Bishop <39043451+MattyyB@users.noreply.github.com>
2018-11-18 16:05:51 -0600

    update dir structure

---
+++ server.js
@@ -0,0 +1,28 @@
+// Dependencies
+require('dotenv').config();
+const express = require('express');
+const path = require('path');
+const routes = require('./controller/loginRoutes');
+const expressValidator = require('express-validator');
+
+// Express setup
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+// View engine
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+
+// Middleware
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(expressValidator());
+
+// Routes
+app.use(routes);
+
+// Server start
+app.listen(PORT, () => {
+    console.log(`--> Server running on http://localhost:${PORT}/`);
+});
