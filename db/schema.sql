aab7dda2a35b637ced73f3abcd78b22dff6b57d1
Jason Michael <michael.jason@outlook.com>
2018-11-18 21:09:57 -0600

    changed schema password type to binary

--- db/schema.sql
+++ db/schema.sql
@@ -7,7 +7,7 @@ CREATE TABLE users
     id  INT(11) NOT NULL AUTO_INCREMENT,
     username VARCHAR(20) NOT NULL UNIQUE,
     email VARCHAR(100) NOT NULL UNIQUE,
-    password VARCHAR(100) NOT NULL,
+    password BINARY(100) NOT NULL,
 
     PRIMARY KEY (id)
 );
