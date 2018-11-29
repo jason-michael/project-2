f0bcdfe7ec79e487c522272a30fdaa388395d5f5
Jason Michael <michael.jason@outlook.com>
2018-11-28 19:43:54 -0600

    added JawsDB database config

--- config/db_connection.js
+++ config/db_connection.js
@@ -1,13 +1,16 @@
 const mysql = require('mysql');
-const dotenv = require('dotenv');
+let connection;
 
-
-const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-});
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+    });
+}
 
 connection.connect(err => {
     if (err) throw err
